dd8d1a9913966b28b266eb82c58e7cb9c895f4a2
chayanin rojarat <chayaninrojarat@gmail.com>
2021-02-01 15:00:42 +0700

    API Categories

--- server/godkoz-api/src/routes/categories.route.ts
+++ server/godkoz-api/src/routes/categories.route.ts
@@ -2,11 +2,10 @@ import { Router, Request, Response } from 'express';
 import * as Categories from '../controllers/categories.controller';
 import Validator from '../utils/middlewares/Validator';
 import * as Schema from '../utils/middlewares/categoriesSchema';
-
+import isAuthenticated from 'src/utils/middlewares/isAuthenticated';
 const router = Router();
 
-router.get('/get-all-categories', Categories.GetAllCategories());
-// router.get('/addcategories/:title', Categories.AddCategories());
+router.get('/', isAuthenticated, Categories.GetAllCategories());
 router.post(
   '/add-categories',
   Validator(Schema.AddCategorySchema),
